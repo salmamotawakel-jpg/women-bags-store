ba29fc0b10c4037567fa1dd250650c78a8c7467f
jomana435 <jomanafarid87@gmail.com>
2026-06-07 20:08:22 +0200

    fix build issues (tailwind + sanity)

--- sanity.config.ts
+++ sanity.config.ts
@@ -6,7 +6,7 @@
 
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
+import { deskTool } from 'sanity/desk'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './sanity/env'
@@ -20,9 +20,9 @@ export default defineConfig({
   // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
   plugins: [
-    structureTool({structure}),
-    // Vision is for querying with GROQ from inside the Studio
-    // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
-  ],
+  deskTool({
+    structure,
+  }),
+  visionTool({ defaultApiVersion: apiVersion }),
+],
 })
